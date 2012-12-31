fd27e9372c185e5941315bd3e77fedd8e83e2bdf
Javier Cordero <jneight@gmail.com>
2012-12-31 19:57:36 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,20 @@
     Original repo: https://github.com/Vizzuality/cartodb-python
 """
 
-#from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 REQUIRES = ['oauth2', 'gevent', 'ujson']
 
 setup(name='cartodb',
       author='Javi Santana',
       author_email='jsantana@vizzuality.com',
+      maintainer='Javier Cordero',
+      maintainer_email='jcorderomartinez@gmail.com',
       description='client to access cartodb api using geventhttpclient',
       version='0.6-gevent',
       url='https://github.com/yoinup/cartodb-python',
       install_requires=REQUIRES,
-      packages=['cartodb'],
+      packages=find_packages(),
       requires=REQUIRES,
       test_suite='test.client')
